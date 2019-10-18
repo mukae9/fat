5ec85de9879e750e8c3aad0eb3d123e76898abeb
mukae9 <rockjoint2016@gmail.com>
2019-10-19 00:36:11 +0900

    フォーム追加

--- lib/main.dart
+++ lib/main.dart
@@ -28,29 +28,109 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _message;
+  @override
+  void initState(){
+    _message='嘘偽りない体重を記録';
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('App Name'),
+        title: new Text('fatrol'),
+
       ),
       body:
-      new Center(
-        child:
-        new Center(
-          child:
-          new Text(
-            "体重管理アプリ",
-            style: new TextStyle(fontSize:12.0,
-                color: const Color(0xFF000000),
-                fontWeight: FontWeight.w200,
-                fontFamily: "Roboto"),
-          ),
+          Column(
+            crossAxisAlignment:CrossAxisAlignment.stretch,
+            children:<Widget>[
 
-        ),
+              Expanded(
+                child:Container(
+                  color: Color.fromARGB(0, 0, 0, 0),
+                  child:Padding(
+                    padding: EdgeInsets.all(25.0),
+                  child: new Text(
+                    "体重管理アプリ\nFatrol", textAlign: TextAlign.center,
+                    style: new TextStyle(fontSize:16.0,
+                    color: const Color(0xFF000000),
+                    fontWeight: FontWeight.w300,
+                        //  decoration: TextDecoration.underline,
+                    fontFamily: "Roboto"),
+                   ),
 
-      ),
+                ),
+                ),
+              ),
+              Expanded(
+                child:Padding(
+                  padding: EdgeInsets.all(25.0),
+                    child:Container(
+                      color: Color.fromARGB(0, 0, 0, 0),
+                      child: new Text(
+                      _message,
+                        textAlign: TextAlign.center,
+                      style: new TextStyle(fontSize:16.0,
+                      color: const Color(0xFF000000),
+                      fontWeight: FontWeight.w300,
+                      //  decoration: TextDecoration.underline,
+                      fontFamily: "Roboto"),
+
+                      ),
+                    ),
+              ),
+
+              ),
+              Expanded(
+              child:Padding(
+                padding: EdgeInsets.all(25.0),
+                  child:TextField(
+                    style: new TextStyle(fontSize:12.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                  ),
+
+              ),
+              ),
+              FlatButton(key:null,
+                  onPressed:buttonPressed,
+                  child:
+                  Text(
+                    "送信",
+                    style: TextStyle(fontSize:12.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "Roboto"),
+                  )
+              ),
+            ]
+          )
+
+
+//      new Center(
+//        child:
+//        new Center(
+//          child:
+//          new Text(
+//            "体重管理アプリ\nFatrol",
+//            textAlign: TextAlign.center,
+//            style: new TextStyle(fontSize:16.0,
+//                color: const Color(0xFF000000),
+//                fontWeight: FontWeight.w300,
+////                decoration: TextDecoration.underline,
+//                fontFamily: "Roboto"),
+//          ),
+//
+//        ),
+//
+//      ),
 
     );
+  }
+  void buttonPressed(){
+    setState(() {
+       _message="…そのままでいいと思ってんの？";
+    });
   }
 }
