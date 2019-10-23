ca9382ca39721606d458ced31d67c0066b357e07
mukae9 <rockjoint2016@gmail.com>
2019-10-23 13:31:43 +0900

    ページ遷移の実装

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,9 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Roboto',
       ),
       home: new MyHomePage(),
+//      home: new MyHomePage(),
     );
+
   }
 }
 
@@ -97,11 +99,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
             ),
 
-               SizedBox(
+            SizedBox(
 
-                  height: 200.0,
-                   child: Center(
-                   child: MyRenderBoxWidget(),
+                height: 200.0,
+                child: Center(
+                  child: MyRenderBoxWidget(),
                 )),
 
             Padding(
@@ -140,25 +142,34 @@ class _MyHomePageState extends State<MyHomePage> {
 
           ]
       ),
+
+
       bottomNavigationBar: new BottomNavigationBar(
-        currentIndex: _index,
-        items: [
-          new BottomNavigationBarItem(
-            icon: const Icon(Icons.create),
-            title: new Text('Add'),
+        currentIndex: 1,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create),
+            title: Text('Add'),
           ),
 
-          new BottomNavigationBarItem(
-            icon: const Icon(Icons.assessment),
-            title: new Text('Graf'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assessment),
+            title: Text('Graf'),
           ),
 
-          new BottomNavigationBarItem(
-            icon: const Icon(Icons.person),
-            title: new Text('Mydate'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Mydate'),
           )
         ],
-        onTap: tapBottomIcon,
+        onTap: (int value){
+          if(value == 1)
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondPage()),
+            );
+
+        },
 
       ),
 
@@ -188,8 +199,8 @@ class _MyHomePageState extends State<MyHomePage> {
   void textChanged(double val){
     setState(() {
 //      _message=val.toUpperCase();
-        _value=val.floorToDouble();
-        _message= '嘘偽りない体重を記録\n$_value kg';
+      _value=val.floorToDouble();
+      _message= '嘘偽りない体重を記録\n$_value kg';
 
     });
   }
@@ -197,7 +208,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void buttonPressed(){
     setState(() {
 //       _message=controller.text + "Kg…\n"+"そのままでいいと思ってんの？";
-         _message= '$_value Kg…\nそのままでいいと思ってんの？';
+      _message= '$_value Kg…\nそのままでいいと思ってんの？';
     });
   }
 
@@ -209,6 +220,198 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 }
+
+
+class SecondPage extends StatefulWidget {
+  SecondPage({Key key}) : super(key: key);
+  @override
+  _SecondPageState createState() => new _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  String _message;
+  double _value = 50.0;
+  int _index = 0;
+  final controller = TextEditingController();
+
+  @override
+  void initState() {
+    _message = '２ページ目\n$_value kg';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('fatrol'),
+
+      ),
+      body:
+      Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+
+            Expanded(
+              child: Container(
+                color: Color.fromARGB(0, 0, 0, 0),
+                child: Padding(
+                  padding: EdgeInsets.all(25.0),
+                  child: new Text(
+                    "体重管理アプリ\nFatrol", textAlign: TextAlign.center,
+                    style: new TextStyle(fontSize: 16.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w300,
+                        //  decoration: TextDecoration.underline,
+                        fontFamily: "Roboto"),
+                  ),
+
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(25.0),
+                child: Container(
+                  color: Color.fromARGB(0, 0, 0, 0),
+                  child: new Text(
+                    _message,
+
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(fontSize: 16.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w300,
+                        //  decoration: TextDecoration.underline,
+                        fontFamily: "Roboto"),
+
+                  ),
+                ),
+              ),
+
+            ),
+
+            SizedBox(
+
+                height: 200.0,
+                child: Center(
+                  child: MyRenderBoxWidget(),
+                )),
+
+            Padding(
+              padding: EdgeInsets.all(25.0),
+              child: Slider(
+                key: null,
+                onChanged: textChanged,
+                value: _value,
+                min: 20.0,
+                max: 100.0,
+
+              ),
+//                  child:TextField(
+//                    controller: controller,
+//                    onChanged: textChanged,
+//                    style: new TextStyle(fontSize:12.0,
+//                        color: const Color(0xFF000000),
+//                        fontWeight: FontWeight.w200,
+//                        fontFamily: "Roboto"),
+//                  ),
+
+            ),
+
+
+            FlatButton(key: null,
+                onPressed: buttonPressed,
+                child:
+                Text(
+                  "送信",
+                  style: TextStyle(fontSize: 12.0,
+                      color: const Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"),
+                )
+            ),
+
+          ]
+      ),
+
+
+      bottomNavigationBar: new BottomNavigationBar(
+        currentIndex: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create),
+            title: Text('???'),
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assessment),
+            title: Text('Graf'),
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Mydate'),
+          )
+        ],
+        onTap: (int value){
+          if(value == 0)
+            Navigator.pop(
+              context
+            );
+
+        },
+
+      ),
+
+
+//      new Center(
+//        child:
+//        new Center(
+//          child:
+//          new Text(
+//            "体重管理アプリ\nFatrol",
+//            textAlign: TextAlign.center,
+//            style: new TextStyle(fontSize:16.0,
+//                color: const Color(0xFF000000),
+//                fontWeight: FontWeight.w300,
+////                decoration: TextDecoration.underline,
+//                fontFamily: "Roboto"),
+//          ),
+//
+//        ),
+//
+//      ),
+
+    );
+  }
+
+
+  void textChanged(double val){
+    setState(() {
+//      _message=val.toUpperCase();
+      _value=val.floorToDouble();
+      _message= '嘘偽りない体重を記録\n$_value kg';
+
+    });
+  }
+
+  void buttonPressed(){
+    setState(() {
+//       _message=controller.text + "Kg…\n"+"そのままでいいと思ってんの？";
+      _message= '$_value Kg…\nそのままでいいと思ってんの？';
+    });
+  }
+
+  void tapBottomIcon(int value){
+    var items = ['追加ページ','グラフページ','マイページ'];
+    setState((){
+      _index = value;
+      _message ='you tapped:"'+items[_index]+'".';
+    });
+  }
+}
+
 
 class MyRenderBoxWidget extends SingleChildRenderObjectWidget {
   @override
